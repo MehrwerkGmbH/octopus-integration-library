3a4a1414516b3484a3c01f131f9617552caf1749
Gürcan Dinler <143404748+gdinler@users.noreply.github.com>
2025-07-16 15:25:18 +0200

    OS-457 Removed copy paste code

--- axa-client-library/src/main/java/de/mehrwerk/octopus/axa/model/enums/CommunicationType.java
+++
@@ -1,8 +0,0 @@
-package de.mehrwerk.octopus.account.model.enums;
-
-public enum CommunicationType {
-    TELEPHONE,
-    FAX,
-    EMAIL,
-    CELLPHONE
-}
--- axa-client-library/src/main/java/de/mehrwerk/octopus/axa/model/enums/OwnershipType.java
+++
@@ -1,7 +0,0 @@
-package de.mehrwerk.octopus.account.model.enums;
-
-public enum OwnershipType {
-    NONE,
-    OWNER,  // Contract owner
-    MEMBER  // Bevollmächtigter
-}
