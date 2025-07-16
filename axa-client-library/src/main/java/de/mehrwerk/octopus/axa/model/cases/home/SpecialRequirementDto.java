f454468e54be39c18c0134fe03786e81e9012034
Gürcan Dinler <143404748+gdinler@users.noreply.github.com>
2025-07-16 15:11:55 +0200

    OS-457 special requirement is added

---
+++ axa-client-library/src/main/java/de/mehrwerk/octopus/axa/model/cases/home/SpecialRequirementDto.java
@@ -0,0 +1,23 @@
+package de.mehrwerk.octopus.axa.model.cases.home;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+/**
+ * Represents special requirements
+ */
+@Data
+@Accessors(chain = true)
+public class SpecialRequirementDto {
+    /**
+     * <p>
+     * boolean
+     * <p>
+     * Indicates if the contact requested to have a checkup of its installation performed once the assistance is finished</p>
+     */
+    @JsonProperty("has_requested_check_up")
+    @NotNull
+    public Boolean hasRequestedCheckUp;
+}
