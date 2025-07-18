d6de7151f4e9a99012c48a2aa6ef40debc709b9f
Gürcan Dinler <143404748+gdinler@users.noreply.github.com>
2025-07-18 09:58:47 +0200

    OS-457 Token response updated

--- axa-client-library/src/main/java/de/mehrwerk/octopus/axa/model/JwtTokenResponse.java
+++ axa-client-library/src/main/java/de/mehrwerk/octopus/axa/model/JwtTokenResponse.java
@@ -1,5 +1,6 @@
 package de.mehrwerk.octopus.axa.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,13 +18,21 @@ public class JwtTokenResponse {
     /**
      * <p>Access token for the JWT</p>
      */
+    @JsonProperty("access_token")
     private String accessToken;
     /**
      * <p>Type of the token, typically "Bearer"</p>
      */
+    @JsonProperty("token_type")
     private String tokenType;
     /**
      * <p>Expiration time of the token in seconds</p>
      */
+    @JsonProperty("expires_in")
     private Long expiresIn;
+    /**
+     * <p>Scope of the token, defining the permissions granted</p>
+     */
+    @JsonProperty("scope")
+    private String scope;
 }
