63582e0819603ccaabc4f129b6dfea9fa39dab04
Gürcan Dinler <143404748+gdinler@users.noreply.github.com>
2025-07-18 09:58:15 +0200

    OS-457 Deserializers updated to handle direct string parsing

--- axa-client-library/src/main/java/de/mehrwerk/octopus/axa/jackson/deserializer/PolicyDeserializer.java
+++ axa-client-library/src/main/java/de/mehrwerk/octopus/axa/jackson/deserializer/PolicyDeserializer.java
@@ -1,10 +1,7 @@
 package de.mehrwerk.octopus.axa.jackson.deserializer;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.*;
 import de.mehrwerk.octopus.axa.model.cases.home.AbstractPolicyDto;
 import de.mehrwerk.octopus.axa.model.cases.home.PolicyEligibilityTokenDto;
 import de.mehrwerk.octopus.axa.model.cases.home.PolicyPartnerIdDto;
@@ -16,17 +13,24 @@ import java.io.IOException;
  * Custom deserializer for {@link AbstractPolicyDto} to handle different policy types based on the presence of specific fields.
  */
 public class PolicyDeserializer extends JsonDeserializer<AbstractPolicyDto> {
+    private static final ObjectMapper mapper = new ObjectMapper()
+            .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+
     @Override
     public AbstractPolicyDto deserialize(JsonParser p, DeserializationContext ctxt)
             throws IOException {
         JsonNode node = p.getCodec().readTree(p);
 
+        if (node.isTextual()) {
+            node = mapper.readTree(node.asText());
+        }
+
         if (node.has("external_policy_number") && node.has("partner_reference") && node.has("package_id")) {
-            return p.getCodec().treeToValue(node, PolicyPartnerReferenceDto.class);
+            return mapper.treeToValue(node, PolicyPartnerReferenceDto.class);
         } else if (node.has("external_policy_number") && node.has("partner_id") && node.has("package_id")) {
-            return p.getCodec().treeToValue(node, PolicyPartnerIdDto.class);
+            return mapper.treeToValue(node, PolicyPartnerIdDto.class);
         } else if (node.has("policy_id") && node.has("eligibility_token")) {
-            return p.getCodec().treeToValue(node, PolicyEligibilityTokenDto.class);
+            return mapper.treeToValue(node, PolicyEligibilityTokenDto.class);
         }
 
         throw new JsonMappingException(p, "Cannot determine Policy type from JSON");
--- axa-client-library/src/main/java/de/mehrwerk/octopus/axa/jackson/deserializer/PolicyHolderDeserializer.java
+++ axa-client-library/src/main/java/de/mehrwerk/octopus/axa/jackson/deserializer/PolicyHolderDeserializer.java
@@ -1,10 +1,7 @@
 package de.mehrwerk.octopus.axa.jackson.deserializer;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.*;
 import de.mehrwerk.octopus.axa.model.cases.home.AbstractPolicyHolderDto;
 import de.mehrwerk.octopus.axa.model.cases.home.PolicyHolderCompanyDto;
 import de.mehrwerk.octopus.axa.model.cases.home.PolicyHolderPersonalDto;
@@ -15,15 +12,22 @@ import java.io.IOException;
  * Custom deserializer for {@link AbstractPolicyHolderDto} to handle different policyholder types based on the presence of specific fields.
  */
 public class PolicyHolderDeserializer extends JsonDeserializer<AbstractPolicyHolderDto> {
+    private static final ObjectMapper mapper = new ObjectMapper()
+            .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+
     @Override
     public AbstractPolicyHolderDto deserialize(JsonParser p, DeserializationContext ctxt)
             throws IOException {
         JsonNode node = p.getCodec().readTree(p);
 
+        if (node.isTextual()) {
+            node = mapper.readTree(node.asText());
+        }
+
         if (node.has("company_legal_name") || node.has("company_registration")) {
-            return p.getCodec().treeToValue(node, PolicyHolderCompanyDto.class);
+            return mapper.treeToValue(node, PolicyHolderCompanyDto.class);
         } else if (node.has("first_name") || node.has("last_name") || node.has("person_registration")) {
-            return p.getCodec().treeToValue(node, PolicyHolderPersonalDto.class);
+            return mapper.treeToValue(node, PolicyHolderPersonalDto.class);
         }
 
         throw new JsonMappingException(p, "Cannot determine PolicyHolder type from JSON");
